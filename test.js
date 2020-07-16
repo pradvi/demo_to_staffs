958da56e48a9b41a562b6fdf171c1c87b07bf07f
pradvi <54692160+pradvi@users.noreply.github.com>
2020-07-16 13:30:18 +0530

    16jul2020changes

---
+++ test.js
@@ -0,0 +1,3 @@
+console.log('hello world');
+console.log(window.innerHeight);
+console.log(window.location.toString);
---
+++ test.ts
@@ -0,0 +1,5 @@
+var myname : string;
+myname="true";
+var hobbies:string[];
+hobbies="['Dance','Song']"
+console.log(myname);
